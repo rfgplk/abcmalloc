84c9d37888b50d7c2cc00616b6bde770d767aaca
rfgplk <rfgplk@users.noreply.github.com>
2026-02-05 20:44:48 +0100

    +

--- src/tapi.hpp
+++ src/tapi.hpp
@@ -33,8 +33,8 @@
 namespace abc
 {
 micron::mutex __global_mutex;
-thread_local static bool __abcmalloc_init = false;
-thread_local static __arena *__main_arena = nullptr;
+static bool __abcmalloc_init = false;
+static __arena *__main_arena = nullptr;
 
 __attribute__((noinline))
 void __thread_init(void){
@@ -49,7 +49,7 @@ void __thread_init(void){
 inline __attribute__((constructor(101))) void
 __global_abcmalloc_start(void)
 {
-  if constexpr ( __default_global_instance ) {
+  if constexpr ( __default_global_instance and __default_construct_on_start ) {
     thread_local static __arena local_arena;
     __main_arena = &local_arena;
     __abcmalloc_init = true;
