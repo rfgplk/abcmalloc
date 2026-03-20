a6ae96d44f1b2728fefb8f4ab64379fdc66dbfd6
rfgplk <rfgplk@users.noreply.github.com>
2026-03-20 21:01:40 +0100

    + tombstone fix

--- tests/extensive.cpp
+++ tests/extensive.cpp
@@ -1,11 +1,14 @@
 #include "../src/cmalloc.hpp"
 
+#include <iostream>
+
 int
 main()
 {
   // comp with debug_notices = true to verify correct patterns
-  for ( u64 i = 0; i < 10'000'000; ++i ) {
-    abc::free(abc::malloc(i*100));
+  for ( u64 i = 0; i < 100'000'000; ++i ) {
+    abc::free(abc::malloc(i));
   }
+  std::cout << "Done" << std::endl;
   return 0;
 }
