60fd3afdd4d86daf390d28f91ccb1055091f3559
rfgplk <rfgplk@users.noreply.github.com>
2026-05-14 15:15:57 +0200

    +push version

--- src/version.hpp
+++ src/version.hpp
@@ -26,7 +26,7 @@ namespace abc
 {
 
 constexpr static const int ABCMALLOC_VERSION_MAJOR = 0x100;
-constexpr static const int ABCMALLOC_VERSION_MINOR = 0x010;
+constexpr static const int ABCMALLOC_VERSION_MINOR = 0x020;
 constexpr static const int ABCMALLOC_VERSION_PATCH = 0x000;
 
 template <int __major, int __minor, int __patch>
@@ -45,6 +45,6 @@ get_version(void)
 }
 
 static_assert(MICRON_VERSION_MAJOR == 0x0000, "! invalid micron version, could result in UB");
-static_assert(MICRON_VERSION_MINOR == 0x0600, "! invalid micron version, could result in UB");
+static_assert(MICRON_VERSION_MINOR == 0x0900, "! invalid micron version, could result in UB");
 
 };
